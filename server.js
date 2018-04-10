a5ab1eee7d18ba685022db72aa9a1a3cd7342ae6
Davinson C <davinsony@gmail.com>
2018-04-09 23:00:23 -0500

    websocket chat

--- server.js
+++ server.js
@@ -11,15 +11,36 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const wss = new SocketServer({ server });
+const webSocketServer = new SocketServer({ server });
+    webSockets = {} // userID: webSocket
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+// CONNECT /:userID
+// wscat -c ws://localhost:5000/1
+webSocketServer.on('connection', function (webSocket) {
+  var userID = parseInt(webSocket.upgradeReq.url.substr(1), 10)
+  webSockets[userID] = webSocket
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(webSockets))
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+  // Forward Message
+  //
+  // Receive               Example
+  // [toUserID, text]      [2, "Hello, World!"]
+  //
+  // Send                  Example
+  // [fromUserID, text]    [1, "Hello, World!"]
+  webSocket.on('message', function(message) {
+    console.log('received from ' + userID + ': ' + message)
+    var messageArray = JSON.parse(message)
+    var toUserWebSocket = webSockets[messageArray[0]]
+    if (toUserWebSocket) {
+      console.log('sent to ' + messageArray[0] + ': ' + JSON.stringify(messageArray))
+      messageArray[0] = userID
+      toUserWebSocket.send(JSON.stringify(messageArray))
+    }
+  })
+
+  webSocket.on('close', function () {
+    delete webSockets[userID]
+    console.log('deleted: ' + userID)
+  })
+})
